cfda03dd0c456b9bac3caa1b523f3d8ae297a297
Christian Lobach <Christian.Lobach@gmail.com>
2015-05-24 21:25:37 +0200

    sync project with filesystem

---
+++ IBANBICRechner/IBANBICRechner/Views/CopyableLabel.swift
@@ -0,0 +1,13 @@
+//
+//  CopyableLabel.swift
+//  IBANBICRechner
+//
+//  Created by Christian Lobach on 24/05/15.
+//  Copyright (c) 2015 Christian Lobach. All rights reserved.
+//
+
+import Cocoa
+
+class CopyableLabel: UILabel {
+
+}
